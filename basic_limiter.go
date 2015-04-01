3c7617c5f03aff67a9d8fbcaefdbd2e20d2804a9
Andrew Stuart <andrew.stuart2@gmail.com>
2015-04-01 08:54:45 -0700

    Move time divisor to const

--- basic_limiter.go
+++ basic_limiter.go
@@ -19,10 +19,14 @@ func (bl basicLimiter) GetLimit() <-chan ByteCount {
 	return bl.cbc
 }
 
+const TIME_UNIT = 50 * time.Millisecond
+
+//BasicLimiter will divvy up the bytes into 100 smaller parts to spread the load
+//across time
 func BasicLimiter(b ByteCount, t time.Duration) Limiter {
 	bl := &basicLimiter{
-		t:   time.NewTicker(t),
-		bc:  b,
+		t:   time.NewTicker(TIME_UNIT),
+		bc:  b / ByteCount(t/TIME_UNIT),
 		cbc: make(chan ByteCount),
 	}
 	go bl.Start()
--- reader.go
+++ reader.go
@@ -2,6 +2,7 @@ package ratelimit
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 )
 
@@ -20,4 +21,45 @@ func NewReader(r io.Reader, l Limiter) io.Reader {
 		br: bufio.NewReader(r),
 		c:  l.GetLimit(),
 	}
+}
+
+type limitedReader struct {
+	br        *bufio.Reader
+	c         <-chan ByteCount
+	eof       bool
+	remaining int
+}
+
+func (lr *limitedReader) Read(p []byte) (written int, err error) {
+	if lr.eof {
+		err = io.EOF
+		return
+	}
+
+	for written <= len(p) {
+		if lr.remaining == 0 {
+			if written > 0 {
+				return
+			}
+
+			lr.remaining = int(<-lr.c)
+		}
+
+		var b byte
+		for i := 0; i < int(lr.remaining) && i < len(p); i++ {
+			b, err = lr.br.ReadByte()
+
+			if err == io.EOF {
+				lr.eof = true
+				return
+			}
+
+			p[written] = b
+			lr.remaining--
+		}
+
+	}
+	fmt.Println(written)
+
+	return
 }
