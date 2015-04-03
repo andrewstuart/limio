c29dae9a987879787972bea1c3095a84d82e8ff2
Andrew Stuart <andrew.stuart2@gmail.com>
2015-04-03 09:23:39 -0700

    Remove basic_limiter and update doc.go

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
 //Package limio provides an interface abstraction for rate limiting or flow control
-//of arbitrary io.Readers or io.Writers. Several concrete implementations of Limiters
-//are also provided.
+//of arbitrary io.Readers or io.Writers. A concrete implementation of a handler
+//is also provided for ease of use and reference are also provided.
 package limio
