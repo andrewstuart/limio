a0819f4e6ae10065dc4ac619622ee0d2f541b7cb
Andrew Stuart <andrew.stuart2@gmail.com>
2015-04-03 12:50:39 -0700

    Add more implementation for EqualLimiter

--- limit_manager.go
+++ limit_manager.go
@@ -5,23 +5,41 @@ import (
 	"time"
 )
 
-type LimitManager struct {
-	rmap map[Limiter]<-chan uint64
+//An EqualLimiter is itself a limiter and will evenly distribute the limits
+//it is given across all its managed Limiters.
+type EqualLimiter struct {
+	rmap   map[Limiter]chan uint64
+	rate   <-chan uint64
+	remain uint64
 }
 
-func (rm *LimitManager) run() {
+func (rm *EqualLimiter) run() {
+	for {
+		lim := <-rm.rate
+
+		perChan := uint64(float64(lim) / float64(len(rm.rmap)))
+
+		for _, c := range rm.rmap {
+			go func() {
+				c <- perChan
+			}()
+		}
+	}
 }
 
-func NewReadManager() *LimitManager {
-	rm := LimitManager{
-		rmap: make(map[Limiter]<-chan uint64),
+func NewEqualLimiter() *EqualLimiter {
+	rm := EqualLimiter{
+		rmap: make(map[Limiter]chan uint64),
 	}
 
 	return &rm
 }
 
-func (rm *LimitManager) NewReader(r io.Reader) *Reader {
+//NewReader is a convenience that will automatically wrap an io.Reader with the
+//internal Limiter implementation.
+func (rm *EqualLimiter) NewReader(r io.Reader) *Reader {
 	lr := NewReader(r)
+	rm.ManageLimiter(lr)
 
 	ch := make(chan uint64)
 	lr.LimitChan(ch)
@@ -35,14 +53,17 @@ func (rm *LimitManager) NewReader(r io.Reader) *Reader {
 		delete(rm.rmap, lr)
 	}()
 
-	return nil
+	return lr
 }
 
-func (rm *LimitManager) Limit(n uint64, t time.Duration) {
+func (rm *EqualLimiter) Limit(n uint64, t time.Duration) {
 }
 
-func (rm *LimitManager) LimitChan(<-chan uint64) {
+func (rm *EqualLimiter) LimitChan(<-chan uint64) {
 }
 
-func (rm *LimitManager) Manage(Limiter) {
+//ManageLimiter accepts a Limiter to be managed under the new "scope"
+//established by this parent Limiter.
+func (rm *EqualLimiter) ManageLimiter(lr Limiter) {
+	return
 }
