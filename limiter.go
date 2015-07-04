71152cbe9ea5711f9e933b6f97f887ec6ef72f04
Andrew Stuart <andrew.stuart2@gmail.com>
2015-07-04 16:56:30 -0700

    Add further documentation to the Limiter interface

--- limiter.go
+++ limiter.go
@@ -1,11 +1,17 @@
 package limio
 
-import "io"
-
 //A Limiter is an interface that meters some underlying discretely quantifiable
 //operation with respect to time.
+//
+//The Limit() function, when implemented, should apply a limit to some
+//underlying operation when called. Supporting concurrency is up to the
+//implementer and as such, should be documented. The semantics of the channel
+//are that of a token bucket. The actual integer sent through the channel
+//represents a quantity of operations that can take place. The implementation
+//should be sure to specify its interpretation of the quantity.
+//
+//Unlimit() removes any formerly imposed limits and allows the underlying operation
 type Limiter interface {
-	Limit(chan int) <-chan bool //The channel is useful for knowing that the channel has been unlimited
+	Limit(chan int) <-chan bool //The channel is useful for knowing that the channel has been unlimited. The boolean represents finality.
 	Unlimit()
-	io.Closer
 }
