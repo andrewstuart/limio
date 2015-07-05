68ccf279f1b54eb2c3efa3464a2989c9814fc29f
Andrew Stuart <andrew.stuart2@gmail.com>
2015-07-05 12:23:08 -0700

    Add documentation about the semantics of the `chan bool` returned by Limit

--- limiter.go
+++ limiter.go
@@ -10,7 +10,14 @@ package limio
 //represents a quantity of operations that can take place. The implementation
 //should be sure to specify its interpretation of the quantity.
 //
-//Unlimit() removes any formerly imposed limits and allows the underlying operation
+//Limit() returns a new boolean channel, used to comunicate that the given
+//`chan int` is no longer being used and may be closed.  A false value
+//indicates that the Limiter has not been shut down and may still be acted
+//upon. True indicates that the limiter has been shutdown and any further
+//function calls will have no effect.
+//
+//Unlimit() removes any formerly imposed limits and allows the underlying
+//operation.
 type Limiter interface {
 	Limit(chan int) <-chan bool //The channel is useful for knowing that the channel has been unlimited. The boolean represents finality.
 	Unlimit()
